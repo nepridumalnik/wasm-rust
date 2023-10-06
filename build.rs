a1a430c31850abdd8c0939b960072f4bd3d667ec
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2023-10-06 23:04:30 +0700

    Initial commit

---
+++ build.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::error::Error;
+use std::process::Command;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let path = env::current_dir()?;
+    let pkg_dir = format!("{}/pkg", path.display());
+
+    let status = Command::new("wasm-pack")
+        .args(&["build", "src/wasm_core", "--out-dir", &pkg_dir])
+        .status()
+        .expect("Не удалось выполнить команду 'wasm-pack build'");
+
+    if !status.success() {
+        panic!("Команда 'wasm-pack build' завершилась с ошибкой");
+    }
+
+    Ok(())
+}
