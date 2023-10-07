717fb9dc259f656597af8e1f5042a1b4c3d9f711
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2023-10-08 00:00:21 +0700

    Добавлена обработка форм

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,14 +1,25 @@
-use actix_web::{App, HttpServer};
+mod reg_post;
+
+use actix_web::{middleware::Logger, web, App, HttpServer};
 
 /// Создать обработчик статических файлов
+/// # Примечание
+/// Должно идти самым последним, чтобы не было коллизии маршрутизации
 fn handle_static_files() -> actix_files::Files {
     return actix_files::Files::new("/", "./web/static").index_file("index.html");
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(handle_static_files()))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .route("/signup", web::post().to(reg_post::process_signup))
+            .service(handle_static_files())
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
