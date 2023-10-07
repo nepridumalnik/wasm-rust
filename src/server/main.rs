7661977e0834b34ecca46d2126025caab4b5cea5
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2023-10-07 15:19:36 +0700

    Рефакторинг

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,16 +1,14 @@
 use actix_web::{App, HttpServer};
 
+/// Создать обработчик статических файлов
+fn handle_static_files() -> actix_files::Files {
+    return actix_files::Files::new("/", "./web/static").index_file("index.html");
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(
-            actix_files::Files::new("/", "./web/static")
-                .show_files_listing()
-                .index_file("index.html")
-                .use_last_modified(true),
-        )
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(handle_static_files()))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
