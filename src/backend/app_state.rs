679fbae3a298e9b085b9b46a2f0f5bb91d8fe138
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2023-10-08 21:45:20 +0700

    Добавлено подключение к СУБД

--- src/backend/app_state.rs
+++ src/backend/app_state.rs
@@ -1,13 +1,17 @@
+mod connection;
+
 use std::sync::Mutex;
 
 pub struct AppState {
     pub counter: Mutex<u32>,
+    pub db: connection::Database,
 }
 
 impl AppState {
     pub fn new() -> AppState {
         AppState {
             counter: Mutex::new(0),
+            db: connection::Database::default(),
         }
     }
 }
---
+++ src/backend/app_state/connection.rs
@@ -0,0 +1,59 @@
+use mysql::prelude::Queryable;
+
+use serde::{Deserialize, Serialize};
+
+const CREATE_MAIN_TABLE: &str = r"CREATE TABLE IF NOT EXISTS Users (
+    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(50) NOT NULL,
+    SecondName VARCHAR(50) NOT NULL,
+    Age INT NOT NULL,
+    Male BOOLEAN NOT NULL,
+    Interests TEXT NOT NULL,
+    City VARCHAR(50) NOT NULL,
+    Password VARCHAR(50) NOT NULL,
+    Email VARCHAR(50) NOT NULL UNIQUE,
+    INDEX (Name, Email)
+    ) ENGINE=InnoDB CHARSET=utf8";
+
+#[derive(Serialize, Deserialize)]
+pub struct Connection {
+    pub name: String,
+    pub password: String,
+    pub address: String,
+    pub port: u16,
+}
+
+impl Connection {
+    fn connect(&self) -> String {
+        format!(
+            "mysql://{}:{}@{}:{}/main_database",
+            self.name, self.password, self.address, self.port
+        )
+    }
+}
+
+pub struct Database {
+    pub pool: mysql::Pool,
+}
+
+impl Database {
+    pub fn new(connection: Connection) -> Database {
+        let connect = connection.connect();
+        let pool = mysql::Pool::new(connect.as_str()).unwrap();
+        let db = Database { pool };
+
+        let mut conn = db.pool.get_conn().unwrap();
+        conn.query_drop(CREATE_MAIN_TABLE).unwrap();
+
+        db
+    }
+
+    pub fn default() -> Database {
+        Self::new(Connection {
+            name: "root".to_string(),
+            password: "==PaSsWoRd==".to_string(),
+            address: "localhost".to_string(),
+            port: 3306,
+        })
+    }
+}
